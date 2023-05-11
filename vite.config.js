6d9582f8f4b3ae3c626bc65fafda012dbcff8249
joshmayeda <mayeda@sonoma.edu>
2023-05-11 01:22:08 -0700

    working version 1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, './images') + '/[!.]*',
+          dest: './'
+        },
+      ],
+    })
+  ],
+  base: './',
 })
